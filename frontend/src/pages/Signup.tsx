604abbc9eab7f191fa0028b0242289bf1ea2c9f5
mitiarenx <mitiasoafinaritra@gmail.com>
2025-11-02 21:44:19 +0300

    frontend: signup started

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,4 +1,20 @@
+// import { useCallback, useState } from "react"
+// import { axiosInstance } from "../utils/axios";
+
 function Signup() {
+
+  // const [ isSigningUp, setIsSigningUp ] = useState(false);
+  // const signup = useCallback(async(data) => {
+  //   setIsSigningUp(true);
+  //   try {
+  //     const res = await axiosInstance.post("/signup", data);
+  //   } catch(error) {
+
+  //   } finally {
+
+  //   }
+  // }, []);
+
   return (
 	<div>Signup</div>
   )
